0bf0cb11ae88ea56fbf7dafabcb38551f4d3af16
Keith White <keithwhite@keiths-macbook-pro.local>
2017-05-23 20:03:25 -0400

    created button and linked pages

--- firsapp/ViewController.swift
+++ firsapp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var username: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func login(_ sender: Any) {
+        if username.text != "" && password.text != "" {
+            print("success")
+    
+        } else {
+            print("failed tolog in")
+        }
+    }
 }
+
 
